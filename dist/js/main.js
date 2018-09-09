d302a5987c00b225639df1b95da6b05e68ea8f2d
JonaThe1st <jona.kaess@online.de>
2018-09-09 11:58:32 +0200

    added media queries

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,7 +15,6 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuBtn.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
 
@@ -23,7 +22,6 @@ function toggleMenu() {
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuBtn.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
 
